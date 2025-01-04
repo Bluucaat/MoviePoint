dfdf09ef3c6e6ef208748cfa2037bc43b3e64124
Ribár Krisztián <rybarkristik@gmail.com>
2025-01-04 17:30:59 +0100

    created base project

---
+++ src/main/java/com/bluu/MoviePoint/MoviePointApplication.java
@@ -0,0 +1,13 @@
+package com.bluu.MoviePoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviePointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviePointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bluu/MoviePoint/MoviePointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluu.MoviePoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviePointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
